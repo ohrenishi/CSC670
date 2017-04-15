870d16ccd22f7e3c3434ba9c38bbce11b9141245
jechandler <jechandler00@gmail.com>
2017-04-14 22:17:17 -0700

    Add files via upload

---
+++ Week1AssignmentForm/Week1AssignmentForm/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Week1AssignmentForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SumButton = new System.Windows.Forms.Button();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.DifferenceButton = new System.Windows.Forms.Button();
+            this.ProductButton = new System.Windows.Forms.Button();
+            this.QuotientButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputBox1 = new System.Windows.Forms.TextBox();
+            this.inputBox2 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(511, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "First Number:";
+            // 
+            // SumButton
+            // 
+            this.SumButton.Location = new System.Drawing.Point(16, 111);
+            this.SumButton.Name = "SumButton";
+            this.SumButton.Size = new System.Drawing.Size(75, 23);
+            this.SumButton.TabIndex = 2;
+            this.SumButton.Text = "Sum";
+            this.SumButton.UseVisualStyleBackColor = true;
+            this.SumButton.Click += new System.EventHandler(this.SumButton_Click);
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(225, 53);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(274, 110);
+            this.outputBox.TabIndex = 3;
+            // 
+            // DifferenceButton
+            // 
+            this.DifferenceButton.Location = new System.Drawing.Point(121, 111);
+            this.DifferenceButton.Name = "DifferenceButton";
+            this.DifferenceButton.Size = new System.Drawing.Size(75, 23);
+            this.DifferenceButton.TabIndex = 4;
+            this.DifferenceButton.Text = "Difference";
+            this.DifferenceButton.UseVisualStyleBackColor = true;
+            this.DifferenceButton.Click += new System.EventHandler(this.DifferenceButton_Click);
+            // 
+            // ProductButton
+            // 
+            this.ProductButton.Location = new System.Drawing.Point(16, 140);
+            this.ProductButton.Name = "ProductButton";
+            this.ProductButton.Size = new System.Drawing.Size(75, 23);
+            this.ProductButton.TabIndex = 5;
+            this.ProductButton.Text = "Product";
+            this.ProductButton.UseVisualStyleBackColor = true;
+            this.ProductButton.Click += new System.EventHandler(this.ProductButton_Click);
+            // 
+            // QuotientButton
+            // 
+            this.QuotientButton.Location = new System.Drawing.Point(121, 140);
+            this.QuotientButton.Name = "QuotientButton";
+            this.QuotientButton.Size = new System.Drawing.Size(75, 23);
+            this.QuotientButton.TabIndex = 6;
+            this.QuotientButton.Text = "Quotient";
+            this.QuotientButton.UseVisualStyleBackColor = true;
+            this.QuotientButton.Click += new System.EventHandler(this.QuotientButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Second Number:";
+            // 
+            // inputBox1
+            // 
+            this.inputBox1.Location = new System.Drawing.Point(106, 53);
+            this.inputBox1.Name = "inputBox1";
+            this.inputBox1.Size = new System.Drawing.Size(100, 20);
+            this.inputBox1.TabIndex = 8;
+            // 
+            // inputBox2
+            // 
+            this.inputBox2.Location = new System.Drawing.Point(106, 79);
+            this.inputBox2.Name = "inputBox2";
+            this.inputBox2.Size = new System.Drawing.Size(100, 20);
+            this.inputBox2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 193);
+            this.Controls.Add(this.inputBox2);
+            this.Controls.Add(this.inputBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.QuotientButton);
+            this.Controls.Add(this.ProductButton);
+            this.Controls.Add(this.DifferenceButton);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.SumButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button SumButton;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Button DifferenceButton;
+        private System.Windows.Forms.Button ProductButton;
+        private System.Windows.Forms.Button QuotientButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox inputBox1;
+        private System.Windows.Forms.TextBox inputBox2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ Week1AssignmentForm/Week1AssignmentForm/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week1AssignmentForm
+{
+    public partial class Form1 : Form
+
+    {
+        public Form1()
+           
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            inputBox1.Clear();
+            inputBox2.Clear();
+            outputBox.Clear();
+        }
+
+        private void SumButton_Click(object sender, EventArgs e)
+        {
+            outputBox.Clear();
+            int sum = 0;
+
+            sum = Convert.ToInt32(inputBox1.Text) + Convert.ToInt32(inputBox2.Text);
+
+            outputBox.AppendText("The sum of " + inputBox1.Text + " and " + inputBox2.Text + " is " + Convert.ToString(sum));
+
+        }
+
+        private void exitToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void DifferenceButton_Click(object sender, EventArgs e)
+        {
+            outputBox.Clear();
+            int diff = 0;
+
+            diff = Convert.ToInt32(inputBox1.Text) - Convert.ToInt32(inputBox2.Text);
+
+
+            outputBox.AppendText("The Difference of " + inputBox1.Text + " and " + inputBox2.Text + " is " + Convert.ToString(diff));
+
+        }
+
+        private void ProductButton_Click(object sender, EventArgs e)
+        {
+            outputBox.Clear();
+            int prod = 0;
+
+            prod = Convert.ToInt32(inputBox1.Text) * Convert.ToInt32(inputBox2.Text);
+
+
+            outputBox.AppendText("The Product of " + inputBox1.Text + " and " + inputBox2.Text + " is " + Convert.ToString(prod));
+
+        }
+
+        private void QuotientButton_Click(object sender, EventArgs e)
+        {
+            outputBox.Clear();
+            double quot = 0;
+
+            quot = Convert.ToDouble(inputBox1.Text) / Convert.ToDouble(inputBox2.Text);
+
+
+            outputBox.AppendText("The Quotient of " + inputBox1.Text + " and " + inputBox2.Text + " is " + Convert.ToString(quot));
+
+        }
+    }
+}
